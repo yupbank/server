eb9dd74a68e8a887e46ab4ee0b9c87e9a86469fb
Misha Chornyi <mchornyi@nvidia.com>
2023-10-26 16:01:35 -0700

    Enable custom name on docker container

--- build.py
+++ build.py
@@ -1559,7 +1559,7 @@ def create_docker_build_script(script_name, container_install_dir, container_ci_
             "-w",
             "/workspace/build",
             "--name",
-            "tritonserver_builder",
+            FLAGS.docker_container_name,
         ]
 
         if not FLAGS.no_container_interactive:
@@ -1581,10 +1581,10 @@ def create_docker_build_script(script_name, container_install_dir, container_ci_
 
         # Remove existing tritonserver_builder container...
         if target_platform() == "windows":
-            docker_script.cmd(["docker", "rm", "tritonserver_builder"])
+            docker_script.cmd(["docker", "rm", FLAGS.docker_container_name,])
         else:
             docker_script._file.write(
-                'if [ "$(docker ps -a | grep tritonserver_builder)" ]; then  docker rm -f tritonserver_builder; fi\n'
+                'if [ "$(docker ps -a | grep ' + FLAGS.docker_container_name + ' )" ]; then  docker rm ' + FLAGS.docker_container_name + '; fi\n'
             )
 
         docker_script.cmd(runargs, check_exitcode=True)
@@ -1593,7 +1593,7 @@ def create_docker_build_script(script_name, container_install_dir, container_ci_
             [
                 "docker",
                 "cp",
-                "tritonserver_builder:/tmp/tritonbuild/install",
+                FLAGS.docker_container_name + ":/tmp/tritonbuild/install",
                 FLAGS.build_dir,
             ],
             check_exitcode=True,
@@ -1604,7 +1604,7 @@ def create_docker_build_script(script_name, container_install_dir, container_ci_
                 [
                     "docker",
                     "cp",
-                    "tritonserver_builder:/tmp/tritonbuild/ci",
+                    FLAGS.docker_container_name + ":/tmp/tritonbuild/ci",
                     FLAGS.build_dir,
                 ],
                 check_exitcode=True,
@@ -2388,6 +2388,12 @@ if __name__ == "__main__":
         required=False,
         help="Requires to split CI build into multiple builds. Will generate cmake build script separatelly for each backend",
         default = [ "tritonserver_buildbase", "tritonserver_buildbase_cache0", "tritonserver_buildbase_cache1", ],
+    )
+    parser.add_argument(
+        "--docker-container-name",
+        required=False,
+        help="Requires to split CI build into multiple builds. Will generate cmake build script separatelly for each backend",
+        default = "tritonserver_builder",
     )
 
     FLAGS = parser.parse_args()
